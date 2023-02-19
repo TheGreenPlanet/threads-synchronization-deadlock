a86f4a35a524310fba2fe2d37f1d94d5ccca2113
TheGreenPlanet <48806927+TheGreenPlanet@users.noreply.github.com>
2023-02-19 17:37:34 +0100

    Bounded buffer assignment done

--- mandatory/src/bounded_buffer.c
+++ mandatory/src/bounded_buffer.c
@@ -24,7 +24,12 @@ void buffer_init(buffer_t *buffer, int size) {
   buffer->mutex = psem_init(1);
 
   // TODO: initialize the rest of the buffer struct members.
-
+  buffer->array = array;
+  buffer->size = size;
+  buffer->in = buffer->out = 0;
+  
+  buffer->data = psem_init(0);
+  buffer->empty = psem_init(size);
 }
 
 void buffer_destroy(buffer_t *buffer) {
@@ -37,7 +42,12 @@ void buffer_destroy(buffer_t *buffer) {
   psem_destroy(buffer->mutex);
   buffer->mutex = NULL;
 
-  // TODO: Deallocate any other resources allocated to the bounded buffer.
+  // Deallocate the data semaphore.
+  psem_destroy(buffer->data);
+  buffer->data = NULL;
+  // Deallocate the empty semaphore.
+  psem_destroy(buffer->empty);
+  buffer->empty = NULL;
 
 }
 
@@ -51,12 +61,11 @@ void buffer_print(buffer_t *buffer) {
   printf(" out: %d\n", buffer -> out);
   puts("");
 
-  int i = 0;
-
   // TODO: print all elements of the array.
-
-  // Print element i of the array. 
-  printf("array[%d]: (%d, %d)\n", i, buffer->array[i].a, buffer->array[i].b);
+  for (int i = 0; i<buffer->size; i++) {
+    // Print element i of the array. 
+    printf("array[%d]: (%d, %d)\n", i, buffer->array[i].a, buffer->array[i].b);
+  }
 
   puts("");
   puts("------------------------");
@@ -66,24 +75,36 @@ void buffer_print(buffer_t *buffer) {
 
 void buffer_put(buffer_t *buffer, int a, int b) {
   // TODO: Add the needed synchronization.
+  psem_wait(buffer->empty);
+  psem_wait(buffer->mutex);
 
   // Insert the tuple (a, b) into the buffer. 
   buffer->array[buffer->in].a = a;
   buffer->array[buffer->in].b = b;
+  // Increment the data count by 1
+  psem_signal(buffer->data);
 
   // TODO: Update buffer->in and make sure it wraps around.
+  buffer->in = (buffer->in + 1) % buffer->size;
 
   // TODO: Add the needed synchronization.
+  psem_signal(buffer->mutex);
 }
 
 void buffer_get(buffer_t *buffer, tuple_t *tuple) {
   // TODO: Add the needed synchronization.
+  psem_wait(buffer->data);
+  psem_wait(buffer->mutex);
 
   // Read the tuple (a, b) from the buffer.
   tuple->a = buffer->array[buffer->out].a;
   tuple->b = buffer->array[buffer->out].b;
+  // Increment the empty count
+  psem_signal(buffer->empty);
 
   // TODO: Update buffer->out and make sure it wraps around.
+  buffer->out = (buffer->out + 1) % buffer->size;
 
   // TODO: Add the needed synchronization.
+  psem_signal(buffer->mutex);
 }
